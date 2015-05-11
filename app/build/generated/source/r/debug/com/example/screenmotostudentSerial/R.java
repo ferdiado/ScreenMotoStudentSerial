649a09c08f811134943f1444bd25f97103fab32d
ferdiado <ferdiado@gmail.com>
2015-05-11 16:37:44 +0200

    Cambio de icono, uno provisional con la foto de la moto. Y cambio del nombre de la aplicación a MotoSudent Florida

--- app/build/generated/source/r/debug/com/example/screenmotostudentSerial/R.java
+++ app/build/generated/source/r/debug/com/example/screenmotostudentSerial/R.java
@@ -18,7 +18,8 @@ public final class R {
         public static final int ic_action_airplane_mode_off=0x7f020000;
         public static final int ic_action_network_cell=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int quadre_de_velocitat=0x7f020003;
+        public static final int moto=0x7f020003;
+        public static final int quadre_de_velocitat=0x7f020004;
     }
     public static final class id {
         public static final int RXusb=0x7f090009;
